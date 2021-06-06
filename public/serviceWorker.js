f436f529b889ab654009605944d4da23c531e7c1
Anup yadav <yanup2913@gmail.com>
2021-06-07 00:20:36 +0530

    now dynamic url cached after network request

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -2,25 +2,33 @@ self.addEventListener('install', function(e) {
     e.waitUntil(
       caches.open('reminders').then(function(cache) {
         return cache.addAll([
-          '/static/css/',
+        //   '/static/css/',
           '/common.js',
-          '/static/js/',
-          'favicon.ico',
-          'manifest.json',
-          '/static/media/'
+        //   '/static/js/',
+          '/favicon.ico',
+          '/manifest.json',
+        //   '/static/media/'
         ]);
       })
     );
    });
    
    self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-   
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
+
+    caches.open('reminders').then(function(cache) {
+        return cache.match(event.request).then(function (response) {
+          return response || fetch(event.request).then(function(response) {
+            cache.put(event.request, response.clone());
+            return response;
+          });
+        });
       })
-    );
+   
+    // event.respondWith(
+    //   caches.match(event.request).then(function(response) {
+    //     return response || fetch(event.request);
+    //   })
+    // );
    });
   
   
